41df53c19f72601f498445104f066c9d83eb43ac
thomas0barand <th.barand@gmail.com>
2026-01-26 13:16:47 +0100

    re organize main branch structure

--- scripts/data/download_fma.py
+++ scripts/data/download_fma.py
@@ -4,19 +4,6 @@ import os
 import urllib.request
 
 
-
-# Download SONICS dataset
-local_dir = "datasets/sonics"
-snapshot_download(repo_id="awsaf49/sonics", repo_type="dataset", local_dir=local_dir)
-
-
-zip_path = os.path.join(local_dir, "part_01.zip")
-with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-    zip_ref.extractall(local_dir)
-
-
-
-
 # Download FMA Small dataset
 fma_dir = "data/fma_small"
 os.makedirs(fma_dir, exist_ok=True)
